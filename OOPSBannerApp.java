a2a938dddd988f1d42aa9f37e09c2bebeb85b457
Swetha <sb7652@srmist.edu.in>
2026-02-23 13:57:04 +0530

    Added UC3 OOPS Banner using String.join()
    #	modified:   OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,6 @@
 public class OOPSBannerApp {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
 
         System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
@@ -11,3 +12,28 @@ public class OOPSBannerApp {
         System.out.println("  *****  " + "  *****  " + " **        " + "  ****** ");
     }
 }
+=======
+public static void main(String[] args) {
+System.out.println(String.join(" ",
+        " ***   ", " ***   ", " ***** ", " ***** "));
+
+System.out.println(String.join(" ",
+        "**   ** ", "**   ** ", "**   **", "**    "));
+
+System.out.println(String.join(" ",
+        "**   ** ", "**   ** ", "**   **", "**    "));
+
+System.out.println(String.join(" ",
+        "**   ** ", "**   ** ", "*****  ", " ***  "));
+
+System.out.println(String.join(" ",
+        "**   ** ", "**   ** ", "**     ", "    **"));
+
+System.out.println(String.join(" ",
+        "**   ** ", "**   ** ", "**     ", "    **"));
+
+System.out.println(String.join(" ",
+        " ***   ", " ***   ", "**     ", "***** "));
+    }
+}
+>>>>>>> 737294f (Added UC3 OOPS Banner using String.join())
