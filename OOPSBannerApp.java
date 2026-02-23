044713cfe453115d6cd8e823a9763784ce81760e
Swetha <sb7652@srmist.edu.in>
2026-02-23 15:31:55 +0530

    Converted to String.join version

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,39 +1,47 @@
-public class OOPSBannerApp {
+ public class OOPSBannerApp {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
 
-        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
-        System.out.println(" **   ** " + " **   ** " + " **    **" + " **      ");
-        System.out.println("**     **" + "**     **" + "**     **" + "**       ");
-        System.out.println("**     **" + "**     **" + "******** " + " ******  ");
-        System.out.println("**     **" + "**     **" + "**        " + "       **");
-        System.out.println(" **   ** " + " **   ** " + " **        " + " **    **");
-        System.out.println("  *****  " + "  *****  " + " **        " + "  ****** ");
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**     "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**     "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "****** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**     ",
+                "     **"));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                "**     ",
+                "     **"));
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                "**      ",
+                " ***** "));
     }
 }
-=======
-public static void main(String[] args) {
-System.out.println(String.join(" ",
-        " ***   ", " ***   ", " ***** ", " ***** "));
-
-System.out.println(String.join(" ",
-        "**   ** ", "**   ** ", "**   **", "**    "));
-
-System.out.println(String.join(" ",
-        "**   ** ", "**   ** ", "**   **", "**    "));
-
-System.out.println(String.join(" ",
-        "**   ** ", "**   ** ", "*****  ", " ***  "));
-
-System.out.println(String.join(" ",
-        "**   ** ", "**   ** ", "**     ", "    **"));
-
-System.out.println(String.join(" ",
-        "**   ** ", "**   ** ", "**     ", "    **"));
-
-System.out.println(String.join(" ",
-        " ***   ", " ***   ", "**     ", "***** "));
-    }
-}
->>>>>>> 737294f (Added UC3 OOPS Banner using String.join())
