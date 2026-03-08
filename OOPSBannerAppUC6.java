b26dca0fb73755d6ba4145328eba9a0940d9e055
Swetha <sb7652@srmist.edu.in>
2026-03-08 22:19:12 +0530

    Added Uc6

--- OOPSBannerAppUC6.java
+++ OOPSBannerAppUC6.java
@@ -1,3 +1,66 @@
+/**
+ * OOPSBannerApp UC6 – OOPS Banner Application (Use Case 6)
+ * This use case extends UC5 by implementing a modular approach
+ * using static helper methods.
+ */
+
 public class OOPSBannerAppUC6 {
 
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+            "  ***  ",
+            " *   * ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " *   * ",
+            "  ***  "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Get patterns using methods
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Loop to print OOPS banner
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
+        }
+    }
 }
